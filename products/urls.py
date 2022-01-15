143d2b28e4a0f8dc306c2772e8337e8ac3b014bb
Hoda Elbadrawe <h.elbadrawe@gmail.com>
2022-01-15 23:35:58 +0200

    change product route

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from products import views
 
 urlpatterns = [
-    path('products', views.ProductList.as_view()),
+    path('', views.ProductList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
