7d44f5dde93e6145f466ffae7cfa55181832aabd
Hoda Elbadrawe <h.elbadrawe@gmail.com>
2022-01-24 02:56:19 +0200

    fix error

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from products import views
 
 urlpatterns = [
-    path('', views.ProductList.as_view()),
+    path('', views.ProductList.as_view, name='product_get_or_post'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
