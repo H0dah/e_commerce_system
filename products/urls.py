606258f9abe79770939546e7f32ec5cfbca11996
Hoda Elbadrawe <h.elbadrawe@gmail.com>
2022-01-25 09:50:08 +0200

    edit product urls.py

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from products import views
 
 urlpatterns = [
-    path('', views.ProductList.as_view, name='product_get_or_post'),
+    path('', views.ProductList.as_view(), name='product'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
